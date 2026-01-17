9071ca34e89fa53b8ff2dfa2216b898b7a74c5c9
Eric Anane Ababio <anane2020@gmail.com>
2026-01-17 02:09:58 +0000

    Base url Updated

--- handlers/process_user_login.php
+++ handlers/process_user_login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once '../includes/db_connection.php';
+require_once '/includes/db_connection.php';
 
 /* ===================== SECURITY ===================== */
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
