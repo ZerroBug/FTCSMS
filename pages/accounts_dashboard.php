c6a3c302cd0a5a2100cb02ffd3eb4e7321bab55c
Eric Anane Ababio <anane2020@gmail.com>
2026-01-24 16:53:12 +0000

    Updated

--- pages/accounts_dashboard.php
+++ pages/accounts_dashboard.php
@@ -10,7 +10,7 @@ require '../includes/db_connection.php';
 if (
     !isset($_SESSION['user_id']) ||
     !isset($_SESSION['user_role']) ||
-    $_SESSION['user_role'] !== 'accountant'
+    $_SESSION['user_role'] !== 'Accountant'
 ) {
     session_unset();
     session_destroy();
