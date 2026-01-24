a122e0d563d34f5ae5b44fded6c6a37d20af3946
Eric Anane Ababio <anane2020@gmail.com>
2026-01-24 16:51:26 +0000

    Updated

--- pages/accounts_dashboard.php
+++ pages/accounts_dashboard.php
@@ -10,7 +10,7 @@ require '../includes/db_connection.php';
 if (
     !isset($_SESSION['user_id']) ||
     !isset($_SESSION['user_role']) ||
-    $_SESSION['user_role'] !== 'Administrator'
+    $_SESSION['user_role'] !== 'accountant'
 ) {
     session_unset();
     session_destroy();
@@ -85,7 +85,7 @@ $learningAreaStats = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 <body>
 
-    <?php include '../includes/administrator_sidebar.php'; ?>
+    <?php include '../includes/accounts_sidebar.php'; ?>
     <?php include '../includes/topbar.php'; ?>
 
     <main class="main">
