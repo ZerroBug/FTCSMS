9c6309dbc79ba55adf65a2a3db7027c49f94eccf
Eric Anane Ababio <anane2020@gmail.com>
2026-01-30 05:51:15 +0000

    Updated

--- pages/fee_configuration.php
+++ pages/fee_configuration.php
@@ -173,10 +173,11 @@ $fee_items = $pdo->query("
                                         <select name="year_group" class="form-select" required>
                                             <option value="">Select</option>
                                             <option value="All">All</option>
-                                            <?php foreach ($year_groups as $yg): ?>
-                                            <option value="<?= $yg['id'] ?>"><?= htmlspecialchars($yg['year_name']) ?>
-                                            </option>
-                                            <?php endforeach; ?>
+                                            <option value="2023">2023</option>
+                                            <option value="2024">2024</option>
+                                            <option value="2025">2025</option>
+                                            <option value="2026">2026</option>
+
                                         </select>
                                     </div>
 
